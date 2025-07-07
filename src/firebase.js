231f2a3c3d8796d7c32356687fa07149ea658e8c
unknown <david123mbah>
2025-07-07 09:34:39 +0100

    design conges and updated screen

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBzLNhWzpYYnzZJHZrcATxAPOsDu4I3vuQ",
-  authDomain: "yourname-1325c.firebaseapp.com",
-  databaseURL: "https://yourname-1325c-default-rtdb.firebaseio.com",
-  projectId: "yourname-1325c",
-  storageBucket: "yourname-1325c.firebasestorage.app",
-  messagingSenderId: "806704872233",
-  appId: "1:806704872233:web:5b697e21fdf8f1c65d1538",
-  measurementId: "G-94D8LNKGQ7"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
